e1337d40631f229ba6e2f88fbd89e92a3301f29b
sam <evaoosyz@gmail.com>
2021-05-29 14:29:39 +0800

    只回Json格式 所以移除XmlFormatter

--- Demo.WebApi/Global.asax.cs
+++ Demo.WebApi/Global.asax.cs
@@ -18,6 +18,8 @@ namespace Demo.WebApi
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //只回Json格式 所以移除Xml
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
 
             AutofacConfig.Register();
         }
